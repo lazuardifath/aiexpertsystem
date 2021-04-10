d60d2d3a0de8333786bc40896da233ad17157849
Lazuardi Fatahilah Hamdi <lazuardifath@gmail.com>
2021-04-10 14:44:56 +0700

    change index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const hbs = require('hbs')
+const app = express()
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
